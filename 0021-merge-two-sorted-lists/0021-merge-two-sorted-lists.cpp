10e46d94b2c595158df1acf6c030dffc1c6a289a
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-02-04 00:43:49 +0530

    Time: 0 ms (100%), Space: 19.6 MB (10.77%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -2,31 +2,25 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
-        ListNode* tempA = a;
-        ListNode* tempB = b;
+        //SC = O(n)  TC = O(nlogn)
         ListNode* c = new ListNode(100);
-        ListNode* tempC = c;
-        while(tempA!=NULL && tempB!=NULL){
-            if(tempA->val<=tempB->val){
-                ListNode* t = new ListNode(tempA->val);
-                tempC->next = t;
-                tempC = t;
-                tempA = tempA->next;
+        ListNode* temp = c;
+        while(a!=NULL && b!=NULL){
+            if(a->val<=b->val){
+                temp->next =a;  //connect c with a
+                a = a->next;
+                temp = temp->next;
 
+            }
+            else{
+                temp->next = b;
+                b  = b->next;
+                temp = temp->next;
+
+            }
         }
-        else{
-            ListNode* t = new ListNode(tempB->val);
-            tempC->next = t;
-            tempC= t;
-            tempB = tempB->next;
-        }
-    }
-        if(tempA==NULL){
-            tempC->next = tempB;
-        }
-        else{
-            tempC->next = tempA;
-        }
+        if(a==NULL) temp->next= b;
+        else temp->next = a;
         return c->next;
 }
 
