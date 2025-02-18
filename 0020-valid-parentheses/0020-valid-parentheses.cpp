804856abb7ab05413cad07e6d2a849c087e313e7
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-02-18 21:43:19 +0530

    Time: 0 ms (100%), Space: 8.9 MB (34.83%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false; // Closing bracket with no opening
+                
+                char top = st.top();
+                st.pop();
+                
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return st.empty(); // If stack is empty, parentheses are balanced
+    }
+};
