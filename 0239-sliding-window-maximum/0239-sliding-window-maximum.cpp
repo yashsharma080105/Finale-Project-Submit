1a2789dfebf6c6a3e1853bbd5caddc2b092cca62
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-03-28 22:07:12 +0530

    Time: 19 ms (78.65%), Space: 139.1 MB (61.92%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-       int n = arr.size();
-       if(k==1) return arr;
-       int ngi[n];
-       stack<int> st;
-       ngi[n-1] = n;
-       st.push(n-1);
-       for(int i=n-2;i>=0;i--){
-        while(st.size()>0 && arr[st.top()]<=arr[i]){
-            st.pop();
-        }
-        if(st.size()==0) ngi[i] = n;
-        else ngi[i] = st.top();
-        st.push(i);
-       }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if(k==1) return nums;
+        int n = nums.size();
+        deque<int> dq;      // decreasing order of ele (index)
+        vector<int> ans;    // i should be min k-1
+        for(int i=0;i<n;i++){
+            while(dq.size()>0 && nums[i] > nums[dq.back()])
+            dq.pop_back();
+            dq.push_back(i);
+            int j = i-k+1;
+            while(dq.front()<j) dq.pop_front();
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
 
-       vector<int> ans;
-       int j = 0;
-       for(int i=0;i<n-k+1;i++){
-        if(j<i) j = i;
-        int mx  = arr[j];   // starting of window se
-        while(j < i+k){   // means if nge is inside window
-            mx = arr[j];
-            if(ngi[j] >= i+k) break;
-            j = ngi[j];
         }
-        ans.push_back(mx);
-       }
-       return ans;
-
+        return ans;
     }
 };
