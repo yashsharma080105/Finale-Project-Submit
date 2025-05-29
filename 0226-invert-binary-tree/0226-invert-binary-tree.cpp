46c97ed1f882fb2a33079730d447bf7d09c7363d
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-05-29 22:53:54 +0530

    Time: 0 ms (100%), Space: 12.6 MB (23.98%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -1,6 +1,7 @@
 
 class Solution {
 public:
+
     void helper(TreeNode* root){
         if(root==NULL) return;
         TreeNode* temp = root->left;
@@ -8,7 +9,9 @@ public:
         root->right = temp;
         helper(root->left);
         helper(root->right);
+        
     }
+    
     TreeNode* invertTree(TreeNode* root) {
         helper(root);
         return root;
