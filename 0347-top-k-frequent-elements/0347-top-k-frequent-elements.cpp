0a0dde0a258312ffe415e72e6b840cb7c258b4c1
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-08-07 11:35:30 +0530

    Time: 0 ms (100%), Space: 17.9 MB (79.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     typedef pair<int,int> pi;
     vector<int> topKFrequent(vector<int>& arr, int k) {
+        int n = arr.size();
         unordered_map<int,int> mp;
-        //map pair is <ele,freq>
-        for(int ele : arr){
+        for(auto ele : arr){
             mp[ele]++;
         }
-        // heap pair is <fre,ele>
+
         priority_queue<pi,vector<pi>,greater<pi>> pq;
-        for(auto x: mp){
-            int ele = x.first , freq = x.second;
+
+        for(auto x:mp){
+            int ele = x.first ;
+            int freq = x.second;
             pair<int,int> p = {freq,ele};
             pq.push(p);
-            // pq.push({x.second,x.first});
             if(pq.size()>k) pq.pop();
         }
+
         vector<int> ans;
         while(pq.size()>0){
             int ele = pq.top().second;
@@ -23,5 +25,6 @@ public:
             pq.pop();
         }
         return ans;
+
     }
 };
