80950bf92ed0984470732d86ffc35c53777e7c4a
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-08-12 00:04:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.01%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
-    int addDigits(int nums) {
-        
-        int sum = 0;
-        
-
-        while(nums>0){
-            int rem = nums%10;
-            sum += rem;
-            nums /= 10;
-
-        }
-        if(sum<10) return sum;
-        else
-            return addDigits(sum);
-
-    
-
-        
+    int addDigits(int num) {
+      if(num==0)
+          return 0;
+      else if(num%9==0)
+          return 9;
+      else
+          return num%9;
     }
 };
