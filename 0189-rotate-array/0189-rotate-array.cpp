5c9a22d6618caf5b76ce7c59e639b526ca2e8420
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-08-11 21:53:16 +0530

    Time: 0 ms (100%), Space: 30.5 MB (9.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    void reversePart(int x,int y, vector<int>& v){
-        while(x<=y){
-            int temp = v[x];
-            v[x] = v[y];
-            v[y] = temp;
-            x++;
-            y--;
-        }
-    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        if(k>n){
-            k = k%n;
-        }
-        reversePart(0,n-k-1,nums);
-        reversePart(n-k,n-1,nums);
-        reversePart(0,n-1,nums);
+        vector<int> ans(n);
 
-        
+        k = k % n; // Handle cases where k > n
+
+        for (int i = 0; i < n; i++) {
+            ans[(i + k) % n] = nums[i];
+        }
+
+        nums = ans; // Copy back to original vector
     }
 };
