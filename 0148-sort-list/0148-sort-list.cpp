66c699c5e356ba67e8f7279bf4163f2738c19ab1
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-08-28 00:35:04 +0530

    Time: 9 ms (88.91%), Space: 58.3 MB (60.82%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,34 +1,31 @@
 
 class Solution {
 public:
-    ListNode* merge(ListNode*a , ListNode* b){
-        //SC = O(1)
+    ListNode* merge(ListNode* a , ListNode* b){
         if(a==NULL) return b;
         if(b==NULL) return a;
-        if(a->val>=b->val) b->next = merge(a,b->next);
-        else{
-            a->next = merge(a->next,b);
-            b = a;
+        if(a->val<=b->val) {a->next = merge(a->next,b); return a;}
+        else {
+            b->next = merge(a,b->next); return b;
         }
-        return b;
-        
     }
+
     ListNode* sortList(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
-        // to find the left middle
         ListNode* slow = head;
         ListNode* fast = head;
         while(fast->next!=NULL && fast->next->next!=NULL){
             slow = slow->next;
             fast = fast->next->next;
         }
-        //now slow is at left middle
         ListNode* a = head;
-        ListNode* b= slow->next;
-        slow->next = NULL;
-        a = sortList(a);
+        ListNode* b = slow->next;
+        slow->next =NULL;
+        a =sortList(a);
         b = sortList(b);
-        ListNode*c = merge(a,b);
+        ListNode* c = merge(a,b);
         return c;
+
+        
     }
 };
