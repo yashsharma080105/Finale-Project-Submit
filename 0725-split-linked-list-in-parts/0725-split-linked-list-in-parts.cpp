053f2e5427a5d50ce244b3f7cba39d9817484569
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-08-29 23:25:16 +0530

    Time: 0 ms (100%), Space: 13.6 MB (83.32%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -2,38 +2,53 @@
 class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
-        //find the length of original linked list
-        int n =0;
         ListNode* temp = head;
+        int n = 0;
         while(temp){
+            
             temp = temp->next;
             n++;
+
         }
-        //breaking the list into k lists
         vector<ListNode*> ans;
         int size = n/k;
         int rem = n%k;
         temp = head;
-        while(temp!=NULL){
-            ListNode* c = new ListNode(100);
+
+        while(temp){
+            ListNode* c = new ListNode(100);   // fake Node
             ListNode* tempC = c;
             int s = size;
-            if(rem>0) s++;
+            if(rem>0) s++; 
             rem--;
             for(int i=1;i<=s;i++){
-                tempC->next  = temp;
+                tempC->next = temp;
                 temp = temp->next;
                 tempC = tempC->next;
+
             }
-            tempC->next = NULL;    //break hogi list
+            if(tempC)
+            tempC->next = NULL;
+            
             ans.push_back(c->next);
+            
         }
         if(ans.size()<k){
-            int extra = k-ans.size();
+            int extra = k - ans.size();
             for(int i=1;i<=extra;i++){
                 ans.push_back(NULL);
             }
         }
         return ans;
+
+
+
+
+
+
+
+
+
+        
     }
 };
