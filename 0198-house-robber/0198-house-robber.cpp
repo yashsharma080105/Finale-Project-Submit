7b3675fe5997b36ad8689a8be51b1b69b07ecc2c
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-09-09 23:48:49 +0530

    Time: 0 ms (100%), Space: 10.1 MB (80.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    vector<int> dp;
     
-
+        // T.C = O(n)  S.c O(1)
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1) return nums[0];
-        dp.resize(105,0);
-        
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        int maxRob  = 0;   // dp[i-1]
+        int prevRob = 0;   // dp[i-2]
+        for(auto currvalue : nums){
+            int temp = max(maxRob,prevRob+currvalue);
+            prevRob = maxRob;
+            maxRob = temp;
 
-        for(int i=2;i<n;i++){
-            dp[i] = max(dp[i-1],nums[i]+dp[i-2]);
+
         }
-        return dp[n-1];
+        return maxRob;
 
        
         
