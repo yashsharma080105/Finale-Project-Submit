b867398b944a4aab7637fea27e5f48477a53c7f8
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-09-09 23:34:24 +0530

    Time: 0 ms (100%), Space: 10.2 MB (73.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<int> dp;
-
-    int f(vector<int> arr, int i){
-        if(i == arr.size()-1) return arr[i];  // single house 
-        if( i == arr.size()-2) return max(arr[i],arr[i+1]);  // 2 house
-        if(dp[i] != -1) return dp[i]; // ith state already computed
-
-    // if dp[i] was - 1 so lets computed ith state for the first
-        return  dp[i] = max(arr[i] + f(arr,i+2), 0+ f(arr,i+1));
-    }
+    
 
     int rob(vector<int>& nums) {
-        dp.resize(105,-1);     // just take a large value in replace of nums.size(
-        return f(nums,0);
+        int n = nums.size();
+        if(n==1) return nums[0];
+        dp.resize(105,0);
+        
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+
+        for(int i=2;i<n;i++){
+            dp[i] = max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
+
+       
         
         
     }
