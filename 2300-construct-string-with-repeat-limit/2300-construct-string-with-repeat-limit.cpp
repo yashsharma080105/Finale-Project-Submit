9c911ca57ff7ac455039480d891e5ea4cd41200d
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-09-15 23:19:56 +0530

    Time: 43 ms (45.06%), Space: 28.2 MB (34.91%) - LeetHub

---
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        priority_queue<pair<char,int>> pq;
+        for(auto p : mp){
+            pq.push(p);
+        }
+        string result = "";
+        while(!pq.empty()){
+            auto largest = pq.top();
+            pq.pop();
+            int len  = min(repeatLimit,largest.second);
+            for(int i=0;i<len;i++){
+                result += largest.first;
+            }
+            pair<char,int> secondLargest;
+            if(largest.second - len >0){
+                if(!pq.empty()){
+                    secondLargest = pq.top();
+                    pq.pop();
+                    result += secondLargest.first;
+                }
+                else{
+                    return result;
+                }
+                if(secondLargest.second -1 > 0 ) pq.push({secondLargest.first,secondLargest.second-1});
+                pq.push({largest.first,largest.second-len});
+            }
+        }
+        return result;
+    }
+};
