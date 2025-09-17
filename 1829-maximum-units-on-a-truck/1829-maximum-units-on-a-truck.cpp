973c0cf4e1a5c63c499bc79fe92c32db32159487
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-09-17 23:04:22 +0530

    Time: 0 ms (100%), Space: 19.8 MB (67.83%) - LeetHub

--- 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
@@ -1,23 +1,27 @@
-bool  cmp(vector<int> &a,vector<int> &b){
-        return a[1]>b[1];
-    }
+bool  cmp(vector<int>& a, vector<int>& b){
+    return a[1]>b[1];
+}
 class Solution {
 public:
     
-    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(),boxTypes.end(),cmp);  //custom comparator
-        int profit = 0;
-        for(int i=0;i<boxTypes.size();i++){
-            if(boxTypes[i][0] <= truckSize){
-            profit += boxTypes[i][0]*boxTypes[i][1];
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize){
+    sort(boxTypes.begin(),boxTypes.end(),cmp);
+    int ans = 0;
+    for(int i=0;i<boxTypes.size();i++){
+        if(boxTypes[i][0]<=truckSize){
+            ans += boxTypes[i][0]*boxTypes[i][1];
             truckSize -= boxTypes[i][0];
+
         }
-        else{
-            profit += truckSize*boxTypes[i][1];
-            truckSize =0;
+        else {
+            ans += truckSize * boxTypes[i][1];
+            truckSize = 0;
         }
         if(truckSize == 0) break;
+
     }
-    return profit;
-    }
+    return ans;
+
+}
+
 };
