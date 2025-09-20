cb340a9af639e0625fe4292f5b0fa35bcb20e6c5
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-09-20 12:21:31 +0530

    Time: 7 ms (21.08%), Space: 12.6 MB (17.15%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<char>> grid;
+    vector<vector<string>> result;
+
+    bool canPlacequeen(int row, int col,int n){
+        // column check
+        for(int i = row-1; i>=0; i--){
+            if(grid[i][col] == 'Q') 
+                return false;
+        }
+        // left diag check
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(grid[i][j] == 'Q') return false;
+        }
+        // right diag check
+        for(int i=row-1, j=col+1; i>=0 && j<n; i--, j++){
+            if(grid[i][j] == 'Q') return false;
+        }
+        return true;   // safe to place queen
+    }
+
+    void f(int row, int n){
+        if(row==n){   // base case
+            vector<string> temp;
+            for(int i=0; i<n; i++){
+                string res = "";
+                for(int j=0; j<n; j++){
+                    res += grid[i][j];
+                }
+                temp.push_back(res);
+            }
+            result.push_back(temp);
+            return;
+        }
+
+        for(int col=0; col<n; col++){
+            if(canPlacequeen(row,col,n)){
+                grid[row][col] = 'Q';
+                f(row+1,n);
+                grid[row][col] = '.';    // backtracking
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n){
+        grid.resize(n, vector<char>(n, '.'));  // clear + initialize
+        result.clear();                        // clear old results
+        f(0,n);
+        return result;
+    }
+};
