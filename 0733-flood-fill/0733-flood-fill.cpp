3121723e4550cd21d310341b5b61e8e43a8373fe
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-09-28 17:14:55 +0530

    Time: 0 ms (100%), Space: 18.1 MB (59%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(color == image[sr][sc]) return image;
+        dfs(image,sr,sc,image[sr][sc],color);
+        return image;
+        
+    }
+
+    // The below function will do dfs from currRow,currCol to its neighbour containing intialcolor
+    void dfs(vector<vector<int>>& image,int currRow,int currCol,int initialColor,int newColor){
+        int  n = image.size() ; // total rows
+        int m = image[0].size(); //toal col
+        if(currRow <0 || currCol<0 || currRow >=n || currCol >=m){
+            return; // you outside the grid
+        }
+
+        // if the cell you landed is not of intial color
+        if(image[currRow][currCol]!= initialColor) return;
+
+        image[currRow][currCol] = newColor;
+        dfs(image,currRow+1,currCol,initialColor,newColor);  // down
+        dfs(image,currRow,currCol-1,initialColor,newColor);   // left
+        dfs(image,currRow-1,currCol,initialColor,newColor);  // up 
+        dfs(image,currRow,currCol+1,initialColor,newColor);  // right
+
+    }
+};
