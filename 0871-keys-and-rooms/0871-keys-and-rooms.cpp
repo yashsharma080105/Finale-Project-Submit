03278066f44283a8f149585fac6e75856240318d
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-09-28 22:13:31 +0530

    Time: 0 ms (100%), Space: 14.7 MB (43.15%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        unordered_set<int> visited;
+        queue<int> qu;
+        qu.push(0);
+        visited.insert(0);
+        while(!qu.empty()){
+            int curr = qu.front();
+            qu.pop();
+            for(int neighbour : rooms[curr]){
+                if(visited.count(neighbour) ==0){
+                    // neighbour not yet visited
+                    qu.push(neighbour);
+                    visited.insert(neighbour);
+                }
+            }
+        }
+        return visited.size() == rooms.size();
+        
+    }
+};
