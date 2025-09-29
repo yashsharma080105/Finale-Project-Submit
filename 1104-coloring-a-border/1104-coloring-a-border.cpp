2571e124c74efcf3fc161227661ac2c077b46574
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-09-29 23:42:27 +0530

    Time: 0 ms (100%), Space: 17.6 MB (95.98%) - LeetHub

--- 1104-coloring-a-border/1104-coloring-a-border.cpp
+++ 1104-coloring-a-border/1104-coloring-a-border.cpp
@@ -1,36 +1,26 @@
 class Solution {
-public:
-    vector<pair<int,int>> internal;
-    void dfs(vector<vector<int>>& grid,int r,int c,int newColor,int originalColor){
-        if(r<0 || c<0 || r>=grid.size() || c>= grid[0].size() || grid[r][c] != originalColor) return;
-        grid[r][c] = -newColor;
-        dfs(grid,r+1,c,newColor,originalColor);
-        dfs(grid,r-1,c,newColor,originalColor);
-        dfs(grid,r,c+1,newColor,originalColor);
-        dfs(grid,r,c-1,newColor,originalColor);
-
-        // below code will execute while coming back from recursion
-        if(not(r==0 || c==0 || r==grid.size()-1 || c==grid[0].size()-1 ||grid[r+1][c] != -newColor || grid[r-1][c] != -newColor || grid[r][c-1] != -newColor || grid[r][c+1] != -newColor)){
-            internal.push_back({r,c}) ;
-        }
-        
+private:
+    void dfs(vector<vector<int>>& grid , int i , int j , int target){
+        if(i < 0 || j < 0 || i == grid.size() || j == grid[0].size() || grid[i][j] != target) return;
+        grid[i][j] = -1;
+        dfs(grid , i - 1 , j , target);
+        dfs(grid , i , j - 1 , target);
+        dfs(grid , i + 1 , j , target);
+        dfs(grid , i , j + 1 , target);
     }
-
+public:
     vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
-        int originalColor = grid[row][col];
-        if(originalColor == color) return grid;
-        dfs(grid,row,col,color,originalColor);
-        for(auto p : internal){
-            int i = p.first;
-            int j = p.second;
-            grid[i][j] = originalColor;
-
-        }
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j] < 0 ) grid[i][j] *=-1;
+        vector<vector<int>> res = grid;
+        dfs(grid , row , col , grid[row][col]);
+        int m = grid.size(), n = grid[0].size();
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(grid[i][j] < 0){
+                    if(i == 0 || j == 0 || i == m - 1 || j == n - 1) res[i][j] = color;
+                    else if(grid[i - 1][j] >= 0 || grid[i][j - 1] >= 0 || grid[i + 1][j] >= 0 || grid[i][j + 1] >= 0) res[i][j] = color;
+                }
             }
         }
-        return grid;
+        return res;
     }
 };
