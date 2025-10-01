a754fd6f852e7c97517d9c8cb07b6485caee83f9
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-10-01 23:53:19 +0530

    Time: 25 ms (72.43%), Space: 16.1 MB (96.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,53 +1,33 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>>& grid,int i,int j){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j] != '1'){
+            return;
+        }
+        grid[i][j] = '0' ;   //mark as visited
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    
+               
+        
+    }
     int numIslands(vector<vector<char>>& grid) {
+        if(grid.empty() || grid[0].empty()) return 0;
         int row  = grid.size();
         int col =  grid[0].size();
 
         int cc = 0;  // store the numbers of connected components
         for(int r=0;r<row;r++){
             for(int c =0;c<col;c++){
-                if(grid[r][c] == '0'){
-                    // it is a water body
-                    continue;
-                }
-                
-                // new univisted land piece found i.e new connected component
-                cc++;
-                grid[r][c] = '0' ; // mark it visited
-                queue<pair<int,int>> qu;
-                qu.push({r,c});  // store the src node
-                while(not qu.empty()){
-                    auto curr = qu.front();  // get one node from queue
-                    qu.pop();
-                    // go to all unvisited neighbour of the curr node
-                    int currRow = curr.first;
-                    int currCol = curr.second;
-                    // check up
-                    if(currRow-1 >=0 and grid[currRow-1][currCol]=='1'){
-                        qu.push({currRow-1,currCol});
-                        grid[currRow-1][currCol] = '0';
-                    }
-                    //check down
-                    if(currRow+1 < row and grid[currRow+1][currCol] == '1'){
-                        qu.push({currRow+1,currCol});
-                        grid[currRow+1][currCol] = '0';
-                    }
-                    //check left
-                    if(currCol-1 >=0 and grid[currRow][currCol-1]=='1'){
-                        qu.push({currRow,currCol-1});
-                        grid[currRow][currCol-1] = '0';
-                    }
-                    //check down
-                    if(currCol+1 < col and grid[currRow][currCol+1] == '1'){
-                        qu.push({currRow,currCol+1});
-                        grid[currRow][currCol+1] = '0';
-                    }
-                    
+                if(grid[r][c] == '1'){
+                    cc++;
+                    dfs(grid,r,c);
                 }
             }
-        }
-        return cc;
-        
+        }return cc;
     }
+
+    
 };
