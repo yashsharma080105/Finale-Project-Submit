78d8a3cf71b1812501eac96f3940a3e4c5628895
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-10-03 23:44:29 +0530

    Time: 7 ms (8.38%), Space: 14.5 MB (61.98%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+
+        vector<vector<int>> dir = {{0,-1},{0,1},{-1,0},{1,0}};
+        queue<pair<int,int>> qu;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if ((i == 0 || j == 0 || i == n-1 || j == m-1) && board[i][j] == 'O') {
+                    qu.push({i, j});
+                    board[i][j] = '#'; // mark as safe
+                }
+            }
+        }
+
+        while(!qu.empty()){
+            auto cell = qu.front();
+            qu.pop();
+            int i = cell.first;
+            int j = cell.second;
+            for(int d =0;d<4;d++){
+                int nr  = i+dir[d][0];
+                int nc = j+dir[d][1];
+               if(nr>=0 and nc>=0 and nr<n and nc<m and board[nr][nc] == 'O'){
+                    board[nr][nc] = '#';
+                    qu.push({nr,nc});
+                }
+            }
+        }
+
+        // 3. Flip the remaining 'O's to 'X', and '#' back to 'O'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == '#') board[i][j] = 'O';
+            }
+        }
+
+        
+    }
+};
