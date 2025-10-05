e3b9237816dd910d6d816b778a11aab9049af6e2
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2025-10-05 18:37:24 +0530

    Time: 3 ms (67.34%), Space: 12.2 MB (46.88%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,51 +1,33 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
+
 
 class Solution {
 public:
     vector<Node*> nodeRegister;
 
-    void dfs(Node* actual , Node* clone){
-        for(auto neighbor : actual->neighbors){
-            if(not nodeRegister[neighbor->val]){
-                // create the neighbor for the first time
-                Node* newNode = new Node(neighbor->val);
+    void dfs(Node* actual,Node* clone){
+        for(auto neighbour : actual->neighbors){
+            if(not nodeRegister[neighbour->val]){
+                Node* newNode = new Node(neighbour->val);
                 nodeRegister[newNode->val] = newNode;
                 clone->neighbors.push_back(newNode);
-                dfs(neighbor,newNode);
+                dfs(neighbour,newNode);
+
             }
             else{
-                clone->neighbors.push_back(nodeRegister[neighbor->val]);
+                clone->neighbors.push_back(nodeRegister[neighbour->val]);
             }
         }
-
     }
-
     Node* cloneGraph(Node* node) {
-        if(node == NULL) return NULL;
-        Node* clone  = new Node(node->val);
-        nodeRegister.resize(110,NULL); // this array contain ref to the created nodes
+        if(node==NULL) return NULL;
+        Node* clone = new Node(node->val);
+        nodeRegister.resize(110,NULL);
         nodeRegister[clone->val] = clone;
         dfs(node,clone);
         return clone;
+
+
+
         
     }
 };
